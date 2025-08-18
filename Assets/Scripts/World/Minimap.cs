4a39b16639b48220ff63c15c30ca8755e1cf9eb1
aicovergod <lewisshuffle136@gmail.com>
2025-08-18 11:13:32 +0100

    Add code-generated top-right minimap

---
+++ Assets/Scripts/World/Minimap.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace World
+{
+    /// <summary>
+    /// Generates a simple minimap in the top-right corner showing a top-down view of the map.
+    /// Everything is created via code so no prefabs or scene objects are required.
+    /// </summary>
+    public class Minimap : MonoBehaviour
+    {
+        private Camera mapCamera;
+        private RenderTexture mapTexture;
+        private Transform target;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void CreateInstance()
+        {
+            var go = new GameObject("Minimap");
+            DontDestroyOnLoad(go);
+            go.AddComponent<Minimap>();
+        }
+
+        private void Awake()
+        {
+            CreateCamera();
+            CreateUI();
+        }
+
+        private void CreateCamera()
+        {
+            mapTexture = new RenderTexture(256, 256, 16)
+            {
+                name = "MinimapTexture"
+            };
+
+            var camGO = new GameObject("MinimapCamera");
+            camGO.transform.SetParent(transform, false);
+            mapCamera = camGO.AddComponent<Camera>();
+            mapCamera.orthographic = true;
+            mapCamera.orthographicSize = 25f;
+            mapCamera.clearFlags = CameraClearFlags.SolidColor;
+            mapCamera.backgroundColor = Color.black;
+            mapCamera.cullingMask &= ~LayerMask.GetMask("UI");
+            mapCamera.targetTexture = mapTexture;
+        }
+
+        private void CreateUI()
+        {
+            var canvasGO = new GameObject("MinimapCanvas", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
+            canvasGO.transform.SetParent(transform, false);
+            var canvas = canvasGO.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            var scaler = canvasGO.GetComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+
+            const int size = 128;
+            const int border = 4;
+
+            var borderGO = new GameObject("Border", typeof(Image));
+            borderGO.transform.SetParent(canvasGO.transform, false);
+            var borderImg = borderGO.GetComponent<Image>();
+            borderImg.color = Color.white;
+            var borderRect = borderImg.rectTransform;
+            borderRect.anchorMin = new Vector2(1f, 1f);
+            borderRect.anchorMax = new Vector2(1f, 1f);
+            borderRect.pivot = new Vector2(1f, 1f);
+            borderRect.sizeDelta = new Vector2(size + border * 2, size + border * 2);
+            borderRect.anchoredPosition = new Vector2(-10f, -10f);
+
+            var rawGO = new GameObject("Image", typeof(RawImage));
+            rawGO.transform.SetParent(borderGO.transform, false);
+            var rawImg = rawGO.GetComponent<RawImage>();
+            rawImg.texture = mapTexture;
+            var rawRect = rawImg.rectTransform;
+            rawRect.anchorMin = Vector2.zero;
+            rawRect.anchorMax = Vector2.one;
+            rawRect.offsetMin = new Vector2(border, border);
+            rawRect.offsetMax = new Vector2(-border, -border);
+        }
+
+        private void LateUpdate()
+        {
+            if (target == null)
+            {
+                var player = GameObject.FindGameObjectWithTag("Player");
+                if (player != null)
+                    target = player.transform;
+            }
+
+            if (target != null && mapCamera != null)
+            {
+                var pos = target.position;
+                mapCamera.transform.position = new Vector3(pos.x, pos.y, -10f);
+            }
+        }
+    }
+}
