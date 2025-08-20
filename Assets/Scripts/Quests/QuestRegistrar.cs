41804ff5a1a8449fafaa223d462406c0c6919a8c
aicovergod <lewisshuffle136@gmail.com>
2025-08-20 22:54:16 +0100

    uwu

---
+++ Assets/Scripts/Quests/QuestRegistrar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Quests
+{
+    public class QuestRegistrar : MonoBehaviour {
+        public QuestDefinition[] quests;
+        void Start() {
+            foreach (var q in quests)
+                QuestManager.Instance.RegisterQuest(q);
+        }
+    }
+}
