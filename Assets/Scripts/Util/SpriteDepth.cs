ca99f5aace34b2eb7d137bc89ab907ff51a0c6ee
aicovergod <lewisshuffle136@gmail.com>
2025-08-21 19:41:14 +0100

    sprite depth

---
+++ Assets/Scripts/Util/SpriteDepth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+    /// Attach to sprites that should auto‑sort by Y-position.
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteDepth : MonoBehaviour
+    {
+        public int offset;            // small positive/negative tweak if needed
+        private SpriteRenderer sr;
+
+        void Awake()
+        {
+            sr = GetComponent<SpriteRenderer>();
+        }
+
+        void LateUpdate()
+        {
+            // Larger (more negative) Y => lower sorting order => appears behind
+            sr.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100f) + offset;
+        }
+    }
+}
