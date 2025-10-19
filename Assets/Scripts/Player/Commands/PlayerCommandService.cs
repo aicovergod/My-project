bf05a8cc0bf4749461cf95706f2bba3c5d5f5c1d
aicovergod <lewisshuffle136@gmail.com>
2025-10-19 11:20:29 +0100

    Add admin command to clear banks

--- Assets/Scripts/Player/Commands/PlayerCommandService.cs
+++ Assets/Scripts/Player/Commands/PlayerCommandService.cs
@@ -168,6 +168,7 @@ namespace Player.Commands
         private void RegisterBuiltInCommands()
         {
             RegisterCommand(new BankCommand());
+            RegisterCommand(new ClearBankCommand());
             RegisterCommand(new ClearInventoryCommand());
             RegisterCommand(new TeleportCommand());
         }
