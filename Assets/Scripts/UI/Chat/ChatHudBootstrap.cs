8d3f795a19affda91df4a92ed9786f462169360f
aicovergod <lewisshuffle136@gmail.com>
2025-10-22 18:27:19 +0100

    Gate chat HUD bootstrap to allowed scenes

--- Assets/Scripts/UI/Chat/ChatHudBootstrap.cs
+++ Assets/Scripts/UI/Chat/ChatHudBootstrap.cs
@@ -2,6 +2,7 @@ using Player;
 using Player.Chat;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using World;
 using Object = UnityEngine.Object;
 
 namespace UI.Chat
@@ -44,17 +45,30 @@ namespace UI.Chat
         private void OnEnable()
         {
             SceneManager.sceneLoaded += HandleSceneLoaded;
-            EnsureHud();
-            TryAttachHudToPlayer();
+            PersistentSceneGate.SceneEvaluationChanged += HandleSceneEvaluation;
+
+            if (PersistentSceneGate.IsActiveSceneAllowed)
+            {
+                EnsureHud();
+                TryAttachHudToPlayer();
+            }
+            else
+            {
+                DestroyHudInstance();
+            }
         }
 
         private void OnDisable()
         {
             SceneManager.sceneLoaded -= HandleSceneLoaded;
+            PersistentSceneGate.SceneEvaluationChanged -= HandleSceneEvaluation;
         }
 
         private void Update()
         {
+            if (!PersistentSceneGate.IsActiveSceneAllowed)
+                return;
+
             if (Time.unscaledTime < nextProbeTime)
                 return;
 
@@ -64,12 +78,40 @@ namespace UI.Chat
 
         private void HandleSceneLoaded(Scene scene, LoadSceneMode mode)
         {
+            if (!PersistentSceneGate.IsActiveSceneAllowed)
+            {
+                DestroyHudInstance();
+                return;
+            }
+
             EnsureHud();
             TryAttachHudToPlayer();
         }
 
+        private void HandleSceneEvaluation(Scene scene, bool allowed)
+        {
+            if (scene != SceneManager.GetActiveScene())
+                return;
+
+            if (allowed)
+            {
+                EnsureHud();
+                TryAttachHudToPlayer();
+            }
+            else
+            {
+                DestroyHudInstance();
+            }
+        }
+
         private void EnsureHud()
         {
+            if (!PersistentSceneGate.IsActiveSceneAllowed)
+            {
+                DestroyHudInstance();
+                return;
+            }
+
             if (hudInstance != null)
                 return;
 
@@ -92,6 +134,12 @@ namespace UI.Chat
 
         private void TryAttachHudToPlayer()
         {
+            if (!PersistentSceneGate.IsActiveSceneAllowed)
+            {
+                DestroyHudInstance();
+                return;
+            }
+
             EnsureHud();
             if (hudInstance == null)
                 return;
@@ -126,6 +174,21 @@ namespace UI.Chat
             }
 
             return Object.FindFirstObjectByType<PlayerChatController>(FindObjectsInactive.Include);
+        }
+
+        private void DestroyHudInstance()
+        {
+            if (hudInstance != null)
+            {
+                if (cachedPlayerController != null && cachedPlayerController.HasHud(hudInstance))
+                    cachedPlayerController.SetHud(null);
+
+                var hudObject = hudInstance.gameObject;
+                Object.Destroy(hudObject);
+                hudInstance = null;
+            }
+
+            cachedPlayerController = null;
         }
     }
 }
